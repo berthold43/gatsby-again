6bd4a340cfc6b7a176ecf197b7ad7e7ec89e77b3
berthold43 <thommy.johansson@sydnet.net>
2021-11-17 10:04:14 +0100

    part7

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: "My Super Cool Blog",
+    title: "Betraktelser",
   },
   plugins: [
     "gatsby-plugin-image",
--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,17 @@ import * as React from 'react'
 import Layout from '../components/layout'
 const AboutPage = () => {
   return (
-    <Layout pageTitle="About Me">
+    <Layout pageTitle="Kroppens harmoni">
+<h3>Appolon och Dionysos</h3>
+<p>Under antiken personifierade Apollon den medvetna kontrollen, som kommer till uttryck genom att den övre människan har ett dominerande inflytande i kroppens sträckning. Appolon hade ansvaret för att leda solens lopp i harmoni med världsalltets gång.</p>
+
+<p>Hans bror Dionysos berikade livet genom allt som naturen och instinkterna kunde skänka människan. Han var i högre grad lierad med den nedre människan och förkroppsligade all den rikedom som ligger i jordens egna krafter.</p>
+
+<p>På samma sätt som vi inom oss själva kan iaktta växlingen mellan dessa båda stora apolloniska och dionysiska flöden kan vi i historien se olika perioder då de stått i motsättning eller harmonierat. Också i geografiskt avseende kan vi i vissa länders traditionella danser se hur tonvikten lagts vid den dionysiska vågrörelsen eller den apolloniska kroppssträckninngen.</p>
+
+<p>Vilka möjligheter till dialoger och till ömsesidiga fruktbara utbyten finns det inte! Med vilken utarmning för oss om vi enbart identifierar oss med en av dessa poler och därigenom försöker utplåna den andra!</p>
+
+<p>Såväl till det yttre som till det inre blir det mycket lättare för oss att känna igen och acceptera dessa båda poler om vår mentala närvaro blir fastare förankrad. Den ensam kan omspänna ytterligheterna utifrån en högre belägen utsiktspunkt, där det rena 'jag är' framträder som den oföränderliga bakgrund mot vilken alla förändringar utspelas.</p>
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
     </Layout>
   )
