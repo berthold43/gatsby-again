f306f54b1e70b695d40a17e1e85dadd91c2e0f8d
berthold43 <thommy.johansson@sydnet.net>
2021-12-16 08:28:56 +0100

    mer info

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,6 +13,30 @@ const AboutPage = () => {
 <p>Vilka möjligheter till dialoger och till ömsesidiga fruktbara utbyten finns det inte! Med vilken utarmning för oss om vi enbart identifierar oss med en av dessa poler och därigenom försöker utplåna den andra!</p>
 
 <p>Såväl till det yttre som till det inre blir det mycket lättare för oss att känna igen och acceptera dessa båda poler om vår mentala närvaro blir fastare förankrad. Den ensam kan omspänna ytterligheterna utifrån en högre belägen utsiktspunkt, där det rena 'jag är' framträder som den oföränderliga bakgrund mot vilken alla förändringar utspelas.</p>
+  <h3>Att stå</h3>
+<p>Efterhand som barnets nervsystem och muskler utvecklas kommer det, sedan det lärt sig att sitta, in i en mycket viktig period då det går på alla fyra och då musklerna och ligamenten stärks. Om vi låter barnet få bli kvar i denna period så länge det känns naturlig kommer benen och bäckenet senare att få den riktiga ledförbindelse som är nödvändig för att benen skall bli raka och starka.</p>
+<p>Denna fyrfotaperiod kan vara en kortare eller längre tid hos olika barn och får inte påskyndas av omgivningen. Man bör alltså undvika stöd, speciella skor eller andra hjälpmedel för att få barnet att resa sig eller gå tidigare. De få veckor som man skulle kunna vinna på så sätt kommer annars att få betalas dyrt under hela livet i form av svaga muskler och ligament i fötter, ben och bäcken. Om kroppstyngden alltför tidigt tvingas belasta ben som ännu inte förberetts inifrån kan det medföra deformationer i lederna som blir ännu mer framträdande i vuxen ålder.</p>
+<p>Låt oss alltså ha förtroende för naturen och ge barnet all den tid den behöver för att till sist kunna stå på egna ben. Denna seger, som barnet vunnit av egen kraft, blir då en fast grund inte bara för kroppsbyggnaden utan också för ett mycket djupt självförtroende, förankrat i känslan av att stå fast grundad på sina egna fötter.</p>
+<p>Vid sidan av organismens inre mognad utsätta barnet för en ständig men indirekt uppmuntran genom de modeller för kroppshållningen som människorna i barnets omgivning omedvetet förmedlar. Barnet är från födseln härmare och stimleras av den blotta närvaron av andra människor till att fortsätta att själv erövra den upprätta kroppsställningen. De få kända fallen av barn som vuxit upp bland vilda djur har saknat just denna förebild, och det har visat sig nästan omöjligt för dem att senare lära sig den upprätta kroppsställningen.</p>
+<p>Det är denna upprättstående, lodräta ställning som skall ge oss material för de närmast följande övningarna.</p>
+<p>Hållningsreflexerna, som i den sittande fasen övades från bäckenet, har nu erövrat benen och fötterna. Parallellt med detta har också armar och händer förvärvat en motsvarnde kontroll i sina rörelser. Från och med födelsen har barnet behövt ungefär ett år för att nå fram till detta stadium: att med kroppen stå i en fullständig och stabil upprätt, vertikal ställning. Genom att studera detta kan muskelsystemets hela struktur bli tydligare för oss.</p>
+<h3>Styrka och sensibilitet</h3>
+<p>Till att börja med kan vi notera den stora skillnade mellan armar och ben:</p>
+<h4>-- i jämförelse med de muskulärt kraftiga benen är armarna relativt fint byggda och lätta;</h4>
+<h4>-- medan benen förbinds med bålen genom solida och enkla ledförbindelser förenas armarna med bröstkorgen i ett komplicerat och subtilt samspel där axelleden avlastas genom nyckelbenet och skulderbladet.</h4>
+<p>Å andra sidan kan man såväl hos armarna som hos benen notera en alltmer tilltagande förfining i riktning från bålen ut mot extremiteterna, från axlarna till fingrarna och från höfterna till tårna. Alla fyra lemmarna utvecklas alltså som grenar som har sina rötter och sitt fäste i bålen och förgrenar sig ända ut i händerna och fötterna för att kunna ta kontakt med yttre föremål.</p>
+<p>Armar och ben behövs för att kunna handla, men de är inte nödvändiga för att leva, tänka eller känna.</p>
+<p>Om vi iakttar lemmarna närmare ser vi hur vart och ett av skelettets segment med en genial konstruktion har sin muskulära "motor" en nivå närmare kroppens centrum: fingrarnas motoriska muskler ligger i handflata och underarm, handens i underarmen, som har sina i överarmen till exempel biceps, överarmens har sina på bålen (till exempel bröstmusklerna som täcker bröstet och har sitt ursprung i bröstbenet). Den breda ryggmusklen som gör det möjligt för oss att dra armarna nedåt-inåt mot kroppen, som när man klättrar uppför ett rep, har sitt ursprung i ryggraden.</p>
+<p>Ett liknande studium av benet visar hur foten har sin muskulära motor i underbenet, vilket har sin i låret, som i sin tur har sin motor på bäckenets nivå som till exempel sättesmusklerna baktill eller psoasmusklerna framtill mot ländkotorna.</p>
+<p>En av fördelarna med detta system är att lemmarna avlastas eftersom varje segment ju endast bär den muskuära motorn till ett mer perifert och alltid lättare segment. Underbenet är till exempel lättare än låret och foten lättar än underbenet.</p>
+<p>Vi ser alltså att det inte är möjligt att studera en isolerad muskel, inte ens en del av em lem. Varje del utgör en helhet, allt starkare och mer förenklad ju närmare bålen den ligger och allt lättare och differentierad ju närmare den ligger periferin, händer och fötter.</p>
+<p>Denna hierarki framträder också i motsatt riktning, i nervsystemets nätverk och i den betydelse som motsvarande delar av hjärnan har.Ju närmare händerna och fötterna, desto rikare är nätverket av nerver.</p>
+<p>Det finns alltid en polaritet mellan den muskulära kraften, som gör det möjligt att röra sig och nervsystemets sensibilitet. I lemmarna visar sig sensibilitetens pol i periferin, i händer och fötter, och kraftens pol i musklernas rötter, som förenar dem med bålen.</p>
+ 
+
+
+
+
 
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
     </Layout>
