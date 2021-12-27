036759bec044063507a0730b62c7645073117ab2
berthold43 <thommy.johansson@sydnet.net>
2021-12-27 10:07:53 +0100

    andas

---
+++ src/pages/andas.js
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="Den harmoniska kroppen">
+
+<h3>Att andas</h3> 
+<p>Andningen omspänner och mäter exakt ut hela vår livslängd, från det första skriket till det sista andetaget. Det är redo att fungera från det vi föds. Det är inte vår egen ansträngning och livserfarenhet som gör att  vi kan lita på den, utan det är den djupt liggande vishet som handlar i vårt omedvetna redan under fosterstadiet. Vid födelsen har hjärtat redan slagit sedan länge, och andningen anpassar undan för undan sin rytm till pulsen till dess den stabiliseras omkring förhållandet ett andetag på fyra hjärtslag. Andningens och hjärtverksamhetens regleringscentra ligger intill varandra i förlängda märgen, hjärnans allra djupaste och mest omedvetna skikt.</p>
+<p>Vi behöver aldrig lära oss att andas. Den kunskap som reglerar andningen ligger före all medveten inlärning. Vi kan andas redan när vi föds, precis som vårt hjärta kan slå redan när vi föds. Vårt första skriḱ satte igång en dittills oanvänd men redan helt arbetsduglig funktion, som långsamt förfinas och anpassar sig för att följa varje etapp av människokroppens växande.</p>
+<p>Andningens kemiska funktion är välbekant: att genom gasutbyte tillföra friskt syre till blodet och transportera bort koldioxid. Men andningens rytmiska funktilon är minst lika viktig. Medan hjärtat bestämmer rytmen för blodets cirkulation och därigenom den omedvetna ämnesomsättningen, står andningen i samklang med rytmen i våra handlingar, våra yttre rörelser. På det fysiska planet anpassar den sig till även de finaste nyanser i vår aktivitet.</p>
+<h3>Andning och känslor</h3>
+
+
+
+
+<p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </Layout>
+  )
+}
+export default AboutPage
