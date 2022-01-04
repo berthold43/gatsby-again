3eb28716645602296986afe26dab6341c84e6670
berthold43 <thommy.johansson@sydnet.net>
2022-01-04 07:47:07 +0100

    innehåll

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,160 @@ const IndexPage = () => {
 />
     
 <h3>Innehåll</h3>
+
+<table>
+  <thead>
+    <tr>
+    <th>1.Vi möts</th>
+    </tr>
+    <tr>
+    <th>Ett sökande</th>
+    </tr>
+    <tr>
+    <td>En praktisk metod</td>
+    </tr>
+    <tr>
+      <th>2. Att andas</th>
+     
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th colspan="2">Andning och känslor</th>
+        </tr>
+        <tr>
+      <th colspan="2">Andningen, vår egen terapeut</th>
+        </tr>
+        <tr>
+<th colspan="2">Den centrala övningen</th>
+    </tr>
+    <tr>
+     
+      <th>Frihet</th>
+    </tr>
+    <tr>
+         <td>Mental närvaro</td>
+    </tr>
+    <td>Balans</td>
+     <tr>
+      <th colspan="2">Öviningens frukter</th>
+        </tr>
+  
+    
+    <tr>
+    
+      <td>Enhet</td>
+    </tr>
+    <tr>
+    <td>Andningsmassage</td>
+    </tr>
+    <tr>
+     <th colspan="2">Öviningens gränser</th>
+     </tr>
+    <tr>
+    <th colspan="2">Kompletterande övningar</th>
+    </tr>
+    <tr>
+      <td>Styrda sträckningar</td>
+    </tr>
+    <tr>
+    <td>Gäspningar</td>
+    </tr>
+    <tr>
+    <td>Spontana sträckningar</td>
+    </tr>
+    <tr>
+    <th>3.Att sitta</th>
+    </tr>
+     <tr>
+<th colspan="2">Den centrala övningen</th>
+    </tr>
+    <tr>
+    <td>Balans</td>
+    </tr>
+    <tr>
+    <td>Former och processer</td>
+    </tr>
+    <tr>
+    <td>Frihet</td>
+    </tr>
+    <tr>
+    <td>Mental närvaro</td>
+    </tr>
+    <tr>
+    <td>Att sitta uppmärksamt</td>
+    </tr>
+    <tr>
+      <th colspan="2">Öviningens frukter</th>
+        </tr>
+    <tr>
+    <td>Balanskonstnären</td>
+    </tr>
+    <tr>
+    <td>Hästen och ryttaren</td>
+    </tr>
+    <tr>
+    <td>Den centrala utsiktspunkten</td>
+    </tr>
+    <tr>
+    <td>Burens upplösning</td>
+    </tr>
+    <tr>
+    <td>Den psykiska förvandlingen</td>
+    </tr>
+    <tr>
+     <th colspan="2">Övningens gränser</th>
+     </tr>
+    <tr>
+    <th>4. Att stå</th>
+    </tr>
+    <tr>
+    <td>Styrka och sensibilitet</td>
+    </tr>
+    <tr>
+    <td>Den centrala övningen</td>
+    </tr>
+    <tr>
+    <td>Lodlinjen - Fötterna - Knäna - Höfterna - Bålen - Axlarna och armarna</td>
+    </tr>
+    <tr>
+    <td>Statisk och dynamisk balans</td>
+    </tr>
+
+  </tbody>
+  
+</table>
+
+<table>
+	<thead>
+		<tr>
+		<th>2. Att andas</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+		<td>Andning och känslor</td>
+		</tr>
+		<tr>
+		<td>Andning vår inre terapeut</td>
+		</tr>
+        <tr>
+        <td>Frihet</td>
+        </tr>
+        <tr>
+        <td>Mental närvaro</td>
+        </tr>
+    </tbody>
+    <thead>
+		<tr>
+		<th>2. Att andas</th>
+		</tr>
+	</thead>
+   
+</table>
+
+
+
 <ul>
 <li>Vi möts</li>
 <li>Att andas</li>
